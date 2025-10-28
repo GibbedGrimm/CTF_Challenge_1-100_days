060ccd0345fcd3729f0f7db1b3beac4248bedc86
GibbedGrimm <andry20050809@gmail.com>
2025-10-28 19:51:52 +0300

    2 день

--- tasks/picoCTF/5.Corrupted file/fixFIle.py
+++ tasks/picoCTF/5.Corrupted file/fixFIle.py
@@ -2,7 +2,7 @@
 f = open('file', 'rb').read()
 recovered = "ffd8ff"+f.hex()[6:]
 print(recovered[:16])
-
+print(bytes.hex(f[:8]))
 with open('img.jpeg','wb') as img:
     img.write(bytes.fromhex(recovered))
 
