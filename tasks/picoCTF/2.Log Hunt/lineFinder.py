1e36c70770fc45509029a88a67a3557baed8f4b1
GibbedGrimm <andry20050809@gmail.com>
2025-11-01 00:28:38 +0300

    try

--- tasks/picoCTF/2.Log Hunt/lineFinder.py
+++
@@ -1,5 +0,0 @@
-with open("server.log") as f:
-    logs = [i for i in f.readlines()]
-for i in logs :
-    if "FLAGPART" in i:
-        print(i)
--- tasks/picoCTF/5.Corrupted file/fixFIle.py
+++
@@ -1,8 +0,0 @@
-
-f = open('file', 'rb').read()
-recovered = "ffd8ff"+f.hex()[6:]
-print(recovered[:16])
-print(bytes.hex(f[:8]))
-with open('img.jpeg','wb') as img:
-    img.write(bytes.fromhex(recovered))
-
