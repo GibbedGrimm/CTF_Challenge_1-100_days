f7f88ce57ca6c771c30ee9e9565fb7c985564f7d
GibbedGrimm <andry20050809@gmail.com>
2025-11-06 00:09:07 +0300

    10 day

---
+++ tasks/cryptoHack/9.Working with Fields/solution.py
@@ -0,0 +1,16 @@
+from sympy import *
+n = 991
+g = 209
+
+print(pow(g,-1,n))
+print(isprime(7))
+for i in range(1,7):
+    try:
+        print(pow(i,-1,7),i)
+
+    except:
+        print("no inv")
+print(28150**28150)
+
+
+a = 5555555555555555555
